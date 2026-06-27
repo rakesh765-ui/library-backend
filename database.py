94cf57bde4a16a845947aed689ca43229d654344
rakesh765-ui <rakesh.mcfisro@gmail.com>
2026-06-27 10:06:57 +0530

    Use DATABASE_URL environment variable

--- database.py
+++ database.py
@@ -1,8 +1,8 @@
-
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:Rakesh722765123@db.smqttpqyxurlhangmyjf.supabase.co:5432/postgres"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
